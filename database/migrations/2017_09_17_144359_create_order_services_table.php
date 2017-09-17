c9e696f15207ecab4b9bcb3aff9b9db3a8f19a8e
root <alvarof.alves@hotmail.com>
2017-09-17 11:53:13 -0300

    migrations

--- database/migrations/2017_09_17_144359_create_order_services_table.php
+++ database/migrations/2017_09_17_144359_create_order_services_table.php
@@ -15,6 +15,37 @@ class CreateOrderServicesTable extends Migration
 	{
 		Schema::create('order_services', function(Blueprint $table) {
             $table->increments('id');
+            $table->string('equipamento');
+            $table->string('n_serie');
+            $table->string('p_info');
+            $table->string('p_const');
+            $table->string('s_exec');
+            $table->date('dt_prox_manut');
+            $table->flot('valor_total');
+            $table->flot('valor_desconto');
+            $table->boolean('status');
+            $table->integer('id_tipo_ordem_servico')->unsigned();
+            $table->integer('id_cliente')->unsigned();
+            $table->integer('id_empresa')->unsigned();
+            $table->integer('id_usuario')->unsigned();
+
+            $table->foreign('id_tipo_ordem_servico')
+                ->references('id')->on('type_order_services')
+                ->onDelete('cascade');
+
+            $table->foreign('id_cliente')
+                ->references('id')->on('customers')
+                ->onDelete('cascade');
+
+            $table->foreign('id_empresa')
+                ->references('id')->on('companies')
+                ->onDelete('cascade');
+
+            $table->foreign('id_usuario')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+
 
             $table->timestamps();
 		});
