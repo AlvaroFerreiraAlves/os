9cd55275f07490bc0ea9495f54a84558a84956ed
root <alvarof.alves@hotmail.com>
2017-10-02 13:43:13 -0300

    Register User

---
+++ app/Entities/UserType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entities;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class UserType extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $fillable = [];
+
+    public function users(){
+        return $this->belongsToMany(User::class, 'user_type_users', 'id_tipo_usuario', '	id_usuario');
+    }
+
+}
