f4d03a2bbdb1e3de6f27c41dd810f93f4a268dc6
Alvaro <alvarof.alves@hotmail.com>
2018-04-10 10:01:09 -0300

    view

---
+++ resources/views/order_services/modals/customer.blade.php
@@ -0,0 +1,116 @@
+<div class="modal fade" id="customermodal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- Form Name -->
+                Cliente
+            </div>
+            <div class="modal-body">
+
+                <form class="form-horizontal" id="form-customer-modal">
+                    {{csrf_field()}}
+                    <fieldset>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="nome">Nome:</label>
+                            <div class="col-md-5">
+                                <input id="nome" name="nome" type="text" placeholder=""
+                                       class="form-control input-md"
+                                       required="">
+
+                            </div>
+                        </div>
+
+                        <!-- Select Basic -->
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="tipo_cliente">Tipo de pessoa:</label>
+                            <div class="col-md-5">
+                                <select id="tipo_cliente" name="tipo_cliente" class="form-control">
+                                    <option value="0">Física</option>
+                                    <option value="1">Jurídica</option>
+                                </select>
+                            </div>
+                        </div>
+
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="cnpj_cpf">CNPJ/CPF:</label>
+                            <div class="col-md-5">
+                                <input id="cnpj_cpf" name="cnpj_cpf" type="text" placeholder=""
+                                       class="form-control input-md"
+                                       required="">
+
+                            </div>
+                        </div>
+
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="endereco">Endereço:</label>
+                            <div class="col-md-5">
+                                <input id="endereco" name="endereco" type="text" placeholder=""
+                                       class="form-control input-md"
+                                       value="{{$customers->endereco or old('endereco') }}" required="">
+
+                            </div>
+                        </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="cep">CEP:</label>
+                            <div class="col-md-5">
+                                <input id="cep" name="cep" type="text" placeholder=""
+                                       class="form-control input-md"
+                                >
+
+                            </div>
+                        </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="telefone">Telefone:</label>
+                            <div class="col-md-5">
+                                <input id="telefone" name="telefone" type="text" placeholder=""
+                                       class="form-control input-md"
+                                >
+
+                            </div>
+                        </div>
+
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="celular">Celular:</label>
+                            <div class="col-md-5">
+                                <input id="celular" name="celular" type="text" placeholder=""
+                                       class="form-control input-md"
+                                       required="">
+
+                            </div>
+                        </div>
+
+
+                        <input type="hidden" id="status" name="status" value="1">
+
+
+                        <!-- Button -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="salvar"></label>
+                            <div class="col-md-4">
+                                <button type="button" id="save-customer" name="save-customer" onclick="saveCustomer()" class="btn btn-primary">Salvar</button>
+                            </div>
+                        </div>
+
+                    </fieldset>
+                </form>
+
+            </div>
+            <div class="modal-footer">
+
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ resources/views/order_services/modals/technician.blade.php
@@ -0,0 +1,147 @@
+<div class="modal fade" id="technicianmodal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- Form Name -->
+                technicianmodal
+            </div>
+            <div class="modal-body">
+
+
+
+                            <form class="form-horizontal" method="post" action="{{ route('register')}}">
+                                {{csrf_field()}}
+                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                    <label for="name" class="col-md-4 control-label">Nome</label>
+
+                                    <div class="col-md-6">
+                                        <input id="name" type="text" class="form-control" name="name"
+                                                required autofocus>
+
+                                        @if ($errors->has('name'))
+                                            <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('cpf') ? ' has-error' : '' }}">
+                                    <label for="cpf" class="col-md-4 control-label">CPF</label>
+
+                                    <div class="col-md-6">
+                                        <input id="cpf" type="text" class="form-control" name="cpf"
+                                                required autofocus @if(isset($user)) disabled @endif>
+
+                                        @if ($errors->has('cpf'))
+                                            <span class="help-block">
+                                        <strong>{{ $errors->first('cpf') }}</strong>
+                                    </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <label for="email" class="col-md-4 control-label">E-Mail</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email"
+                                               value="{{$user->email or old('email') }}" required  @if(isset($user)) disabled @endif>
+
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                @if(!isset($user))
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <label for="password" class="col-md-4 control-label">Senha</label>
+
+                                        <div class="col-md-6">
+                                            <input id="password" type="password" class="form-control" name="password" required>
+
+                                            @if ($errors->has('password'))
+                                                <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="password-confirm" class="col-md-4 control-label">Confirmar senha</label>
+
+                                        <div class="col-md-6">
+                                            <input id="password-confirm" type="password" class="form-control"
+                                                   name="password_confirmation" required>
+                                        </div>
+                                    </div>
+                                @endif
+
+                                <div class="form-group{{ $errors->has('endereco') ? ' has-error' : '' }}">
+                                    <label for="endereco" class="col-md-4 control-label">Endereço</label>
+
+                                    <div class="col-md-6">
+                                        <input id="endereco" type="text" class="form-control" name="endereco"
+                                              required autofocus>
+
+                                        @if ($errors->has('endereco'))
+                                            <span class="help-block">
+                                        <strong>{{ $errors->first('endereco') }}</strong>
+                                    </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('telefone') ? ' has-error' : '' }}">
+                                    <label for="telefone" class="col-md-4 control-label">Telefone</label>
+
+                                    <div class="col-md-6">
+                                        <input id="telefone" type="text" class="form-control" name="telefone"
+                                               required autofocus>
+
+                                        @if ($errors->has('telefone'))
+                                            <span class="help-block">
+                                        <strong>{{ $errors->first('telefone') }}</strong>
+                                    </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <!-- Select Multiple -->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="tipousuario">Tipo usuário</label>
+                                    <div class="col-md-4">
+                                        <select id="tipousuario" name="tipousuario[]" class="form-control" multiple="multiple">
+
+
+                                                    <option value="" selected>tecnico</option>
+
+
+                                        </select>
+                                    </div>
+                                </div>
+                                <input type="hidden" id="status" name="status" value="1">
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Salvar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                    </form>
+
+
+            </div>
+            <div class="modal-footer">
+
+            </div>
+        </div>
+
+    </div>
+</div>
